58e1ce985f95ffd17801b1d684a9a8bfd25e7cdf
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:25 +0300

    Add ITestDataFactory interface

---
+++ CicdApp.Data/Seed/ITestDataFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Seed
+{
+    public interface ITestDataFactory
+    {
+        List<BuildStage> CreateBuildStages();
+        List<IssueLog> CreateIssueLogsForBuildStage(int buildStageId, bool hasErrors);
+        List<ThreadSpeedMetric> CreateThreadSpeedMetrics();
+    }
+}
