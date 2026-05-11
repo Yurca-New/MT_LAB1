f98a6fc84283351bc7cb5c71f94fc0abe7b5d16f
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:09 +0300

    Implement ThreadSpeedMetricRepository

---
+++ CicdApp.Data/Repositories/ThreadSpeedMetricRepository.cs
@@ -0,0 +1,13 @@
+﻿using CicdApp.Data.Data;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Repositories
+{
+    public class ThreadSpeedMetricRepository : Repository<ThreadSpeedMetric>, IThreadSpeedMetricRepository
+    {
+        public ThreadSpeedMetricRepository(AppDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
