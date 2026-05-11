546720467a0b48ce4b22eeb0d414042e402a0235
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:09 +0300

    Implement IssueLogRepository

---
+++ CicdApp.Data/Repositories/IssueLogRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CicdApp.Data.Data;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Repositories
+{
+    public class IssueLogRepository : Repository<IssueLog>, IIssueLogRepository
+    {
+        public IssueLogRepository(AppDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<IReadOnlyList<IssueLog>> GetByBuildStageIdAsync(int buildStageId)
+        {
+            return await this.dbSet.Where(il => il.BuildStageId == buildStageId).ToListAsync().ConfigureAwait(false);
+        }
+    }
+}
