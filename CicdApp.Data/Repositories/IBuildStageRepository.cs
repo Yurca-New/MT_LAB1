f307db0f5101cbdb9e0ac37feee98d7f97cdc345
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:02 +0300

    Add IBuildStageRepository interface

---
+++ CicdApp.Data/Repositories/IBuildStageRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Repositories
+{
+    public interface IBuildStageRepository : IRepository<BuildStage>
+    {
+        Task<IReadOnlyList<BuildStage>> GetByStatusAsync(bool isSuccess);
+    }
+}
