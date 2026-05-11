d5cb2479516ecc78b7bd96468649d10276c433da
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:02 +0300

    Add IIssueLogRepository interface

---
+++ CicdApp.Data/Repositories/IIssueLogRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Repositories
+{
+    public interface IIssueLogRepository : IRepository<IssueLog>
+    {
+        Task<IReadOnlyList<IssueLog>> GetByBuildStageIdAsync(int buildStageId);
+    }
+}
