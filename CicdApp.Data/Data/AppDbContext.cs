7248bc7e538cb4060678a6b8814b44e2d26104f4
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:37:48 +0300

    Add AppDbContext with entity configuration

---
+++ CicdApp.Data/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BuildStage> BuildStages { get; set; }
+        public DbSet<IssueLog> IssueLogs { get; set; }
+        public DbSet<ThreadSpeedMetric> ThreadSpeedMetrics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(modelBuilder));
+            }
+
+            modelBuilder.Entity<BuildStage>()
+                .HasMany(bs => bs.IssueLogs)
+                .WithOne(il => il.BuildStage)
+                .HasForeignKey(il => il.BuildStageId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<BuildStage>().HasIndex(bs => bs.WhenStarted);
+            modelBuilder.Entity<IssueLog>().HasIndex(il => il.BuildStageId);
+            modelBuilder.Entity<ThreadSpeedMetric>().HasIndex(tsm => tsm.Timestamp);
+        }
+    }
+}
