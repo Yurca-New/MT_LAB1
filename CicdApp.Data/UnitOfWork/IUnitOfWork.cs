5d9bf3abc8a57053460b60357b4fc264d40e8aee
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:15 +0300

    Add IUnitOfWork interface

---
+++ CicdApp.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using CicdApp.Data.Repositories;
+
+namespace CicdApp.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable, IAsyncDisposable
+    {
+        IBuildStageRepository BuildStages { get; }
+        IIssueLogRepository IssueLogs { get; }
+        IThreadSpeedMetricRepository ThreadSpeedMetrics { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
