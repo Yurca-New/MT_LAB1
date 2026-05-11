5d258674a0fc712161f6ff42d2bf1c264fd4edbc
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:37:39 +0300

    Add IssueLog entity

---
+++ CicdApp.Data/Entities/IssueLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CicdApp.Data.Entities
+{
+    [Table("IssueLogs")]
+    public class IssueLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int BuildStageId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LogLevel { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string ErrorCode { get; set; } = string.Empty;
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime Timestamp { get; set; }
+
+        [ForeignKey(nameof(BuildStageId))]
+        public virtual BuildStage? BuildStage { get; set; }
+    }
+}
