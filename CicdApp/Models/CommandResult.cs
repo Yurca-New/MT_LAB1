842c6a2331ee40ce54b26cf4e479c13eed2c7124
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:31 +0300

    Add CommandResult model

---
+++ CicdApp/Models/CommandResult.cs
@@ -0,0 +1,9 @@
+﻿namespace CicdApp.Models
+{
+    public class CommandResult
+    {
+        public int ExitCode { get; set; }
+        public string Output { get; set; } = string.Empty;
+        public string Error { get; set; } = string.Empty;
+    }
+}
