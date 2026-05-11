d0f6a286314f00d4bd21ebc489aa3b0d6eb9dd30
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:01 +0300

    Add IRepository<T> interface

---
+++ CicdApp.Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CicdApp.Data.Repositories
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
