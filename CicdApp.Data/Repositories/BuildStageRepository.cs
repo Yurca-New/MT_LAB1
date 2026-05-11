df7ced46d68f0748ee1afeb045264bf62879baa2
Yurca <nazaruk.yurii@student.ehu.lt>
2026-05-11 22:38:09 +0300

    Implement BuildStageRepository

---
+++ CicdApp.Data/Repositories/BuildStageRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CicdApp.Data.Data;
+using CicdApp.Data.Entities;
+
+namespace CicdApp.Data.Repositories
+{
+    public class BuildStageRepository : Repository<BuildStage>, IBuildStageRepository
+    {
+        public BuildStageRepository(AppDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<IReadOnlyList<BuildStage>> GetByStatusAsync(bool isSuccess)
+        {
+            return await this.dbSet.Where(bs => bs.IsSuccess == isSuccess).ToListAsync().ConfigureAwait(false);
+        }
+    }
+}
